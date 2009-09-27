cb902b4623bd7e6054efc265bbd38e9f42d0c244
Eldar <wireghoul@justanotherhacker.com>
2009-09-27 19:39:18 +1000

    Testing harness working "more correctly"

--- t/harness.sh
+++ t/harness.sh
@@ -1,15 +1,35 @@
 #!/bin/sh
+# Simple shell based test harness
+
+# Default values
+PLANNED=0
+OK=0
+FAIL=0
+RET=0
+
+plan () {
+    PLANNED=$1
+}
+
+# Track highest return value
+set_ret () {
+    if [ $1 -gt $RET ]; then
+        RET=$1
+    fi
+}
 
 #Test should exit with status code 0 or 1 in accordance with POSIX
-
 ok () {
     echo -n "Testing $1 ... "
     $2 2>/dev/null >/dev/null
     if [ $? -le 1 ]; then
+        OK=$(($OK+1))
         echo "Ok"
     else
+        FAIL=$(($FAIL+1))
         echo "Failed"
     fi
+    set_ret $?
 }
 
 
@@ -17,8 +37,31 @@ not_ok () {
     echo -n "Testing $1 ... "
     $2 2>/dev/null >/dev/null
     if [ $? -lt 2 ]; then
+        FAIL=$(($FAIL+1))
         echo "Failed"
+        # Cannot use value of $? as success is fail in this case
+        set_ret 2
     else
+        OK=$(($OK+1))
         echo "Ok"
+        # Cannot use value of $? as fail is success in this case
+        set_ret 0
     fi
 }
+
+# Run reports and return highest exit code on exit
+die () {
+    TOTAL=$(($OK+$FAIL))
+    if [ $TOTAL -ne $PLANNED ]; then
+        echo "Wrong number of tests ($TOTAL) expected $PLANNED"
+        exit 2
+    fi
+    echo "Completed $TOTAL of $PLANNED tests"
+    echo "OK: $OK"
+    echo "FAIL: $FAIL"
+    exit $RET
+
+}
+
+# Don't make reporting and aggregation optional
+trap 'die' EXIT
--- t/runtests.sh
+++ t/runtests.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
-# Iterate through tests
+# Iterate through testscripts
+
+# Any failed testscript is a make fail
+F=0
 
 for TEST in t-*.sh; do
-    /bin/sh $TEST 
+    /bin/sh $TEST
+    # Allow all tests to run before returning the highest exit code recorded
+    T=$?
+    if [ $T -ne $F ]; then
+        F=$T
+    fi
 done
+exit $F
