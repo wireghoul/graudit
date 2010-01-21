51728e20ffb8ac96b8fc6a40f85ed1e923c6592c
Eldar <wireghoul@justanotherhacker.com>
2010-01-21 20:06:47 +1100

    Added requires to php coverage test

--- t/includes.php
+++ t/includes.php
@@ -1,9 +1,12 @@
 <?php
 //1 vuln, 1 safe call
-include($_GET['lang']."/error100.txt");
+include($_GET['lang'].'/error100.txt');
 include("frameset.php");
 include_once($_COOKIE['theme']."/css3-style.php");
 include_once('menu.inc.php');
-//require
+require($_GET[category]."php");
+require('functions.php');
+require_once($_POST[action]);
+require_once('includes/DB.php');
 //fread
 ?>
