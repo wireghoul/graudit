540b3d8d87217f9573d26e1cfacdaecd167f2ea3
Eldar <wireghoul@justanotherhacker.com>
2010-05-02 14:14:42 +1000

    Reverted to basic before making test suite switch

--- t/runtests.sh
+++ t/runtests.sh
@@ -1,23 +1,14 @@
 #!/bin/sh
 # Iterate through testscripts
 
-TOTAL_TESTS=0
-TOTAL_PLANNED=0
-TOTAL_OK=0
-TOTAL_FAIL=0
-
-# Any failed testscript is a make fail
 F=0
-
 for TEST in t-*.sh; do
     /bin/sh $TEST $*
     # Allow all tests to run before returning the highest exit code recorded
     T=$?
-    if [ $T -ne $F ]; then
+    if [ $T -ge $F ]; then
         F=$T
-        TOTAL_OK=$(($TOTAL_OK + $OK))
     fi
 done
 rm test-logfile
-echo "OK: $TOTAL_OK"
 exit $F
