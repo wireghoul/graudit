452bcb75b4933d38e908c17e4a7f62ce76e34286
Eldar <wireghoul@justanotherhacker.com>
2010-05-08 14:07:58 +1000

    Updated tests to allow for greps 0-1 ok return code

--- t/t-c.sh
+++ t/t-c.sh
@@ -7,5 +7,5 @@ test_description='C signature database tests'
 # Nothing to setup
 
 # Tests
-test_expect_success 'C signature syntax check' '../graudit -d dotnet anotherfile'
+test_expect_code 1 'C signature syntax check' '../graudit -d dotnet anotherfile'
 test_done
