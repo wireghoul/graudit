abf6cf6eebd7438531e37403f553c38689a07074
Eldar <wireghoul@justanotherhacker.com>
2010-05-09 13:40:25 +1000

    Test suite fix

--- t/t-git.sh
+++ t/t-git.sh
@@ -7,5 +7,5 @@ test_description='Graudit repo tests'
 
 # TESTS
 test_expect_success 'No outstanding commits' 'git status|grep "working directory clean"'
-test_expect_success 'No outstanding push/pulls' 'git status|grep -v "# Your branch is"'
+test_expect_code 1 'No outstanding push/pulls' 'git status|grep "# Your branch is"'
 test_done
