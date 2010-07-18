a97f770edb5302581d852eed5b54c9ff760f08dc
Eldar <wireghoul@justanotherhacker.com>
2010-07-18 14:30:25 +1000

    Fixed taint signatures and dupe removal

--- aux/phptaint.sh
+++ aux/phptaint.sh
@@ -3,6 +3,11 @@
 # Written by Wireghoul - http://www.justanotherhacker.com
 # Released under the GPL licence
 VERSION=0.1
+if [ -z $1 ]; then
+    echo "Usage: $0 /path/to/check"
+    exit 2
+fi
 graudit -z -d php $1 | \
-perl -ne 'if ($_ =~ m/\$(.*?)\s*=\s*\$_(GET|POST|REQUEST|COOKIE)\[.*?\]/) { print "\\\$$1"; }' | \
+perl -ne 'if ($_ =~ m/\$(\S+?)\s*=\s*\$_(GET|POST|REQUEST|COOKIE)\[.*?\]/) { print "\\\$$1\n"; }' | \
+sort | uniq | \
 graudit -d - $1
