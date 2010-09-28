bcf90a3792c2c68307c1e8b1247bff8dd9189849
Eldar Marcussen <wireghoul@justanotherhacker.com>
2010-09-28 05:25:13 +0400

    More demo scripts

---
+++ aux/fm-php.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Scrape freshmeats php releases and run graudit on them
+# The wget invocation it unsafe, but renaming files is a pain
+# Written by wireghoul
+GET http://freshmeat.net/tags/php | \
+perl -ne 'if ($_ =~ m{href="(/urls/.*)" class="floatright welcome padleft5 download}) { print "http://freshmeat.net/$1\n"; }' | \
+xargs -n1 wget --use-server-file-name -nc
+#Extract files
+unzip *.zip
+tar zxf *gz
+tar zxf *.tar
+graudit -d php *
---
+++ aux/svnhook.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#!/bin/sh
+# Graudit svnhook example by Wireghoul, uses PHP ruleset
+REPOSITORY=$1
+REVISION=$2
+/usr/local/bin/svnlook diff -r $REVISION $REPOSITORY | \
+graudit -d php - | \
+ansi2html.sh | \
+mail -e -s "[graudit report] rev$REVISION - $REPOSITORY" root@domkain.com
