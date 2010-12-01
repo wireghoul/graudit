b19de3e3522d50ba0f88895d2d19318637351b78
Eldar Marcussen <wireghoul@justanotherhacker.com>
2010-12-01 08:05:30 +0300

    Playing catchup with the test suite

--- t/t-000.sh
+++ t/t-000.sh
@@ -5,9 +5,15 @@ test_description="Invocation checks"
 . ./test-lib.sh
 
 # Tests
-test_expect_success 'version check' '../graudit -v'
 test_expect_code 2 'usage check' '../graudit'
+test_expect_code 1 'Context argument check' '../graudit -c 3 anotherfile'
+test_expect_code 1 'Help text test' '../graudit -h'
+test_expect_code 1 'Case insensitive search' '../graudit -i anotherfile'
 test_expect_success 'list databases' '../graudit -l'
-test_expect_code 1 'exclude files' '../graudit -x *.js anotherfile'
+test_expect_code 1 'Vim lines test' '../graudit -L anotherfile'
+test_expect_success 'version check' '../graudit -v'
+test_expect_code 1 'exclude files check' '../graudit -x *.js anotherfile'
+test_expect_code 1 'Supress colors check' '../graudit -z anotherfile'
+test_expect_code 1 'High contrast color check' '../graudit -Z anotherfile'
 test_expect_code 2 'bad argument' '../graudit -99'
 test_done
