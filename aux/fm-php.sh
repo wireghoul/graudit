ba908d432803877aadd6fdce991664b007212b34
Eldar Marcussen <wireghoul@justanotherhacker.com>
2011-06-01 22:05:10 -0400

    Updated to match the new wget option for trusting filenames on redirect

--- aux/fm-php.sh
+++ aux/fm-php.sh
@@ -4,7 +4,7 @@
 # Written by wireghoul
 GET http://freshmeat.net/tags/php | \
 perl -ne 'if ($_ =~ m{href="(/urls/.*)" class="floatright welcome padleft5 download}) { print "http://freshmeat.net/$1\n"; }' | \
-xargs -n1 wget --use-server-file-name -nc
+xargs -n1 wget --trust-server-name -nc
 #Extract files
 unzip *.zip
 tar zxf *gz
