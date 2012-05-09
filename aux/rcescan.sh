689d024218368e48eb311eb6767cea357af6c992
Eldar Marcussen <wireghoul@justanotherhacker.com>
2012-05-08 23:53:24 -0400

    quick and dirty RCE specific taint checker

---
+++ aux/rcescan.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# PHP taint checking with graudit - PoC script
+# Written by Wireghoul - http://www.justanotherhacker.com
+# Released under the GPL licence
+VERSION=0.1
+if [ -z "$1" ]; then
+    echo "Usage: $0 /path/to/check"
+    exit 2
+fi
+RCEHOME=`dirname $0`
+graudit -z -c 0 -d $RCEHOME/rce.db "$1" | \
+perl -ne 'while ($_ =~ m!\$([a-z0-9-_\[\]\x27\>]+)!gi) { print "\\\$$1\n"; print "include\nrequire\n"; }' | \
+sed -e's/\[/\\[/g' -e's/\]/\\]/g' | \
+sort | uniq | \
+graudit -d - "$1"
+#more
