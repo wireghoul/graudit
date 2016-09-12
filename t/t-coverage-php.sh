9264eb5d0319bc267e93ba724319fe5498b80fa0
Eldar Marcussen <eldar@fruitpwn.local>
2016-09-12 15:55:56 +1000

    Fixed broken test cases

--- t/t-coverage-php.sh
+++ t/t-coverage-php.sh
@@ -5,6 +5,6 @@ test_description='PHP signature tests'
 . ./test-lib.sh
 
 # Tests
-test_expect_code 1 'whitespace test' 'test `../graudit -c 0 -d ../signatures/php.db whitespace.php | wc -l` -eq 2'
-test_expect_code 1 'exec test' 'test `../graudit -c 0 -d ../signatures/php.db exec.php | wc -l` -eq 4'
+test_expect_code 0 'whitespace test' 'test `../graudit -c 0 -d ../signatures/php.db whitespace.php | wc -l` -eq 2'
+test_expect_code 0 'exec test' 'test `../graudit -z -c 0 -d ../signatures/php.db exec.php | wc -l` -eq 13'
 test_done
