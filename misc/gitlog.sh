03c0ff064a2407478bcae001c036d695446da156
Wireghoul <wireghoul@justanotherhacker.com>
2020-10-29 08:54:52 +1100

    Reworked git log script to use lopps rather than xargs

--- misc/gitlog.sh
+++ misc/gitlog.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # gitlog - A graudit augmentation script
 # Written by @Wireghoul - justanotherhacker.com
 # Check recent history for some n-day?
@@ -6,6 +6,9 @@
 
 git --no-pager log --oneline | \
 grep -Ei '(security|sqli|sql inj|xss| rce |command injection|vulnerability|cmdi| lfi |traversal)' | \
-head -$1 | \
-cut -d' ' -f1 | \
-xargs -n1 git --no-pager show 
+head -$1| \
+while read diff; do 
+  # Below line is useful if feeding gitlog back into graudit
+  # printf "Showing: $diff\n======================================================\n" >&2
+  git --no-pager show $(echo $diff|cut -d' ' -f1)
+done
