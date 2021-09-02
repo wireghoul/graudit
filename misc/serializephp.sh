c83c958a2f52a11e34b253050985fa7cdfd605db
Wireghoul <wireghoul@justanotherhacker.com>
2021-09-02 15:59:56 +1000

    Compatibility: Changed interpreter to bash to avoid issues with which echo gets called when path is ommited

--- misc/serializephp.sh
+++ misc/serializephp.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 (echo -ne " __wakeup[[:space:]]*\\(\n __destruct[[:space:]]*\\(\n __autoload[[:space:]]*\\(\n"; \
 # uncomment below if you want everything
 # echo -ne " __toString[[:space:]]*\\(\n __call[[:space:]]*\\(\n __Set[[:space:]]*\\(\n __get[[:space:]]*\\(\n" \
