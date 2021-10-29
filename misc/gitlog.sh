84191a88cdd6014334aa91eca5ad1dea714cfb50
Wireghoul <wireghoul@justanotherhacker.com>
2021-10-29 14:25:08 +1100

    Making gitlog interactive

--- misc/gitlog.sh
+++ misc/gitlog.sh
@@ -4,11 +4,15 @@
 # Check recent history for some n-day?
 # usage: cd repo;gitlog.sh 50
 
-git --no-pager log --oneline | \
+IFS=$'\n' read -r -d '' -a glog < <(git --no-pager log --oneline | \
 grep -Ei '(security|sqli|sql inj|xss| rce |command injection|vulnerability|cmdi| lfi |traversal)' | \
-head -$1| \
-while read diff; do 
+head -$1 && printf '\0')
+for diff in "${glog[@]}"; do 
   # Below line is useful if feeding gitlog back into graudit
   # printf "Showing: $diff\n======================================================\n" >&2
-  git --no-pager show $(echo $diff|cut -d' ' -f1)
+  echo "show $diff ?"
+  read -n1 -s -p $'Press enter to continue...any other key to skip...\n' key
+  if [ "$key" = '' ]; then
+      git --no-pager show $(echo $diff|cut -d' ' -f1)
+  fi
 done
