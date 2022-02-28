c34659e2fa32618f7fa44aa7311ae005ac8e2401
Eldar Marcussen <emarcussen@seek.com.au>
2022-02-28 18:06:16 +1100

    Updated phptaint.sh

--- misc/phptaint.sh
+++ misc/phptaint.sh
@@ -8,6 +8,6 @@ if [ -z "$1" ]; then
     exit 2
 fi
 graudit -z -d php "$1" | \
-perl -ne 'if ($_ =~ m/\$(\S+?)\s*=\s*\$_(GET|POST|REQUEST|COOKIE)\[.*?\]/) { print "\\\$$1\n"; }' | \
+perl -ne 'if ($_ =~ m/\$(\S+?)\s*=\s*\$_(GET|POST|REQUEST|COOKIE|FILES)\[.*?\]/) { print "\\\$$1\n"; }' | \
 sort | uniq | \
 graudit -d /dev/stdin "$1"
